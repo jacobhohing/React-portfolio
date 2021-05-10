efc16a12fbc3a1e30e25cca9095b2243d8e14950
Jacob Hohing <jakehohing@yahoo.com>
2021-05-09 23:02:05 -0400

    Added basic portfolio components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,39 @@
 import React, {Component} from "react";
-import API from "./utils/API.js";
+import ProjCard from './components/ProjCard';
+import projects from './projects.json';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMars } from '@fortawesome/free-solid-svg-icons'
 import { faVenus } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
+ 
   state = {
-    employees: []
+    projects: [...projects]
   }
-
-    handleSort = (key, asc) => {
-
-      let empSorted = [...this.state.employees];
-      empSorted.sort( (a, b) => {
-        return a.name.last > b.name.last ? asc * 1 : asc * -1;
-      });
-
-      this.setState({ employees: empSorted });
-    }
-
-  handleAPI = event => {
-
-    API.getEmployees()
-    .then( data => {
-        let employees = [...data.data.results];
-        console.log(employees);
-        this.setState({employees: employees});
-    });
-  }
-
 
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid">
           
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-              <a className="navbar-brand" href="#">Employee Directory</a>
+              <a className="navbar-brand" href="#">Jake Hohing</a>
             </div>
           </nav>
-          <button className="btn btn-primary mr-2" onClick={() => this.handleSort("name", 1)}>Sort By Last Name</button>
-          <button className="btn btn-primary mr-2" onClick={() => this.handleSort("name", -1)}>Reverse Sort by Last Name</button>
-          
-          <button className="btn btn-success" onClick={() => this.handleAPI()}>Push to Get Employees</button>
-          
-          <table className="table table-striped mt-3">
-            <tbody>
-              {this.state.employees.map((employee, index) => (
-                  <tr key={"Employee" + index}>
-                    <td >
-                      {employee.name.first}
-                    </td>
-                    <td >
-                      {employee.name.last}
-                    </td>
-                    <td >
-                      {employee.gender === 'male' ? <FontAwesomeIcon icon={faMars} /> : <FontAwesomeIcon icon={faVenus} />}
-                    </td>
-                  </tr>
-              ))}
-              </tbody>
-          </table> 
+          {this.state.projects.map( project => (
+          <ProjCard
+              key={project.id}
+              {...project}
+
+            />
+          ))}
+
+          <nav className="navbar fixed-bottom navbar-light bg-light">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">My Links</a>
+            </div>
+          </nav>
         
       </div>
     );
